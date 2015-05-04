08b62f202e4dfa7a21a8d21c34ee066b0fbf066f
Angus Lo <angus.lo@havasww.com>
2015-05-04 17:51:01 -0400

    display child's personality results; display confidence percentage

--- public/js/ui.js
+++ public/js/ui.js
@@ -2,7 +2,9 @@
 
 	$('#pasteCorpusLink').click(showPasteCorpus);
 	$('#results').css('display', 'none').css('opacity', '0');
+	$('#moreInfo').css('display', 'none').css('opacity', '0');
 	$('#results').on('resultsReady', showResults);
+
 
 	function showPasteCorpus() {
 		$('#pasteCorpus').css('display', 'block');
@@ -11,24 +13,34 @@
 	function showResults() {
 		$('#personalityEntry .collapse').collapse();
 		$('#results').css('display', 'block').css('opacity', '1');
+		$('#moreInfo').css('display', 'block').css('opacity', '1');
 
-		formatResults($('#results .schools'), $('#results').data("schools"));
+		formatResults($('#results').data("schools"));
 	}
 
-	function formatResults(list, data) {
+	function formatResults(data) {
+		var schoolsList = $('#results .schools');
+		var personalitiesList = $('#moreInfo .personalities');
+
 		$.each(data.matches, function(i, school) {
 			var li = $('<li/>')
 					.text(school.school_name)
-					.appendTo(list);
+					.appendTo(schoolsList);
 			var traitsText = [];
 			$.each(school.traits, function(j, trait) {
 				if (trait.percentage > 0.75) {
-					traitsText.push(trait.name);
+					traitsText.push(trait.name/* + " (" + (trait.percentage*100).toFixed(2) + "%)"*/);
 				}
 			});
 			var info = $('<p/>')
 						.text("Personality: "+traitsText.join(', '))
 						.appendTo(li);
+		});
+
+		$.each(data.studentPersonality, function(i, personality) {
+			var li = $('<li/>')
+					.text(personality.name + " (" + (personality.percentage*100).toFixed(2) + "%)")
+					.appendTo(personalitiesList);
 		});
 	}
 
